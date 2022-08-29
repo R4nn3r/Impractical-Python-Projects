aec075ac9b8216312110352d63c49a491a643668
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-08-29 14:46:40 +0300

    Aug29, 2022 Projects for python

--- PROJECT #2: FINDING PALINDROMES/palindromes.py
+++ PROJECT #2: FINDING PALINDROMES/palindromes.py
@@ -3,7 +3,16 @@ import load_dictionary
 
 
 def main():
-    ...
+    """ How it works"""
+    word = "ethiopia"
+    
+    # using slicing 
+    rev_word = word[::-1]
+    
+    if word == rev_word:
+        print("Palindrome detected")
+    else:
+        print("Not a palindrome.")
     
     
 if __name__=='__main__':
