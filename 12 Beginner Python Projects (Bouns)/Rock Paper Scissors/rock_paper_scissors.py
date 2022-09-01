f68deadcce6b7c0e84e2c6c161ad567662c537f0
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-01 20:59:00 +0300

    sep 11 2022 .. 12 beginner projects

---
+++ 12 Beginner Python Projects (Bouns)/Rock Paper Scissors/rock_paper_scissors.py
@@ -0,0 +1,21 @@
+'''
+    - create game counter, winner tracker
+    - Enter while loop
+        - get users name and choice from r, p, s
+        - get a random choice from r, p, s list and store in in a variable
+        - if user's input == variable 
+            - add 1 to the game counter
+            - print game tied 
+        - if (r > s, s > p, p > r) based on this rule
+            - add 1 to the winner (computer or user(name))
+            - print the game winner
+        - ask the user to play again
+            - yes
+                repeat
+            - no
+                break out the loop and print out the winner (amount of win per player)
+                
+    Note:
+        must use multiple function's
+        
+ '''
