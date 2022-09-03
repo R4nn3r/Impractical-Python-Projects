de5f718a9f58182394167f22178db98256f7489e
Yonatan Afewerk <yonatanafewerk@gmail.com>
2022-09-03 21:01:17 +0300

    PROJECT #5 kinda done

--- PROJECT #5: FINDING PHRASE ANAGRAMS/anagrams.py
+++ PROJECT #5: FINDING PHRASE ANAGRAMS/anagrams.py
@@ -15,31 +15,40 @@ def get_word():
             sys.exit("Enter a single word (no-space) to get its Anagram!")
     
 
-def get_anagram(word_list, users_word):
-    """ Checking the users word against the list to find all the anagrams"""
-    anagram_list = []
+def get_anagram(word_list, users_name):
+    """ Checking the users word against the list to find all phrases that are anagrams"""
+    phrase_anagram_list = []
+    phrase_len = 0
+    name_len = len(users_name)
+    limit = name_len 
     
-    for word in word_list:
-        if sorted(word) == sorted(users_word):
-            anagram_list.append(word)
+    while phrase_len < limit:
+        ...
+        
+        for word in word_list:
+            if sorted(word) == sorted(users_name):
+                phrase_anagram_list.append(word)
     
-    return anagram_list
+    
+    
+    
+    return phrase_anagram_list
         
 
 def main():
     word_list = load_dictionary.load('2of4brif.txt')
     
-    users_word = get_word()
+    users_name = get_word()
     
     # find words that the inputs anagram
-    print(f"Anagrams found for {users_word}: ")
+    print(f"Anagrams found for {users_name}: ")
     
 
     #display list of anagram list
-    if not get_anagram(word_list,users_word):
-        print(f"No anagram found for {users_word}: ")
+    if not get_anagram(word_list,users_name):
+        print(f"No anagram found for {users_name}: ")
     else:
-        for _ in get_anagram(word_list,users_word):
+        for _ in get_anagram(word_list,users_name):
             print("  ",_)
         
     
